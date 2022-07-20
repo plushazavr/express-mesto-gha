d83d279c1922e1798032dca4ef195e1df183b535
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-07-20 09:44:00 +0300

    feat add model email, password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -6,16 +7,37 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Поле имя должно быть заполнено'],
     minLength: [2, 'Минимальное количество букв в имени - 2'],
     maxLength: [30, 'Минимальное количество букв в имени - 30'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     required: [true, 'Укажите информацию о себе'],
     minLength: [2, 'Минимальное количество букв в описании - 2'],
     maxLength: [30, 'Минимальное количество букв в описании - 30'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
     required: [true, 'Добавьте аватар'],
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Некорректный адрес URL',
+    },
+  },
+  email: {
+    type: String,
+    required: [true, 'Поле email  должно быть заполнено'],
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Некорректный адрес почты',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле пароль должно быть заполнено'],
+    select: false,
   },
 });
 
