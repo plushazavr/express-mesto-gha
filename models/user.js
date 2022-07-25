6bbd9b4f2716bfa4d5d32e96e7a3b991396e2844
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-07-25 11:14:46 +0300

    fix bug err500 with log non-existent user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const { UnauthorizedError } = require('../errors/index-err');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -54,13 +55,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw new UnauthorizedError('Данные пользователя не найдены');
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw new UnauthorizedError('Данные пользователя не найдены');
           }
 
           return user;
