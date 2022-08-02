2d3948e887c36f9cacbfbba3689cc7e5511e712c
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-08-02 10:40:48 +0300

    add cors

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const cors = require('./middlewares/cors');
 const errorHandler = require('./middlewares/errorHandler');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 
 const { validateSignup, validateSignIn } = require('./middlewares/validators');
 const NotFoundError = require('./errors/not-found-err');
-const { logger, requestLogger, errorLogger } = require('./middlewares/logger');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -23,21 +29,16 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(cors);
+app.use(requestLogger); // подключаем логгер запросов
+app.use(limiter);
+app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
-app.use(
-  rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-  }),
-  helmet(),
-);
 
-app.use(logger);
-app.use(requestLogger);
 app.post('/signin', validateSignIn, login);
 app.post('/signup', validateSignup, createUser);
 
@@ -50,6 +51,7 @@ app.use('*', () => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
